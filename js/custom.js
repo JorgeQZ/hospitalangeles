0819193664c26fdd6dfcf7f45382858028dc1c22
JorgeQZ <jorge_qzg@hotmail.com>
2020-04-12 01:44:37 -0500

    email

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,59 @@
 $(document).ready(function () {
+
+    function CheckForm() {
+        let errors = 0;
+        const inputs = $('#contact-form input');
+
+        $('#errors-display').html('');
+        inputs.each(function (index, element) {
+            if (element.value == '' || !element.value) {
+                $(element).addClass('error');
+                errors++;
+            } else if (!element.value == '') {
+                $(element).removeClass('error');
+                $(element).addClass('success');
+            } else {
+                $(element).addClass('success');
+            }
+        });
+
+        if (errors == 1) {
+            $('#errors-display').fadeIn().html('Un campo está vacío.');
+            return false;
+        } else if (errors > 1) {
+            $('#errors-display').fadeIn().html('Algunos campos están vacíos.');
+            return false;
+
+        } else {
+            $('#errors-display').fadeOut().html('');
+            return true;
+        }
+    }
+
+    function SendMail(element) {
+
+
+    }
+    $('#contact-form').bind("submit", function (e) {
+        // e.preventDefault();
+        if (CheckForm()) {
+            // SendMail($(this));
+            let values = $(this).serialize();
+            let type = $(this).attr("method");
+            let url = $(this).attr("action");
+            console.info(values, type, url);
+
+            // $.ajax({
+            //     type: type,
+            //     url: url,
+            //     data: values
+            // });
+
+        }
+        // return 0;
+    });
+
+
     $('.procedimientos-options-container .item').click(function (e) {
         $('.procedimientos-options-container .item').removeClass('active');
         $(this).addClass('active');
