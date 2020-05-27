9ac05f1def3ee0cebfc9b7d9ce93aee7f7b8b9b3
JorgeQZ <jorge_qzg@hotmail.com>
2020-05-27 12:48:43 -0500

    bmi

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@ try {
     $mail->Host       = 'smtp.live.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'jorge_qzg@hotmail.com';                     // SMTP username
-    $mail->Password   = 'Spiderman@23';                               // SMTP password
+    // $mail->Password   = 'Spiderman@23';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
